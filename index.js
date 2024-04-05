4f55172cec3dd29b1d9fbf1bb8af1a04e684cca8
ez1n <eodrmfdl1004@naver.com>
2024-04-06 00:30:10 +0900

    fix: 모듈 수정

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-export * from './utils';
+function sum(a, b) {
+  return a + b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+
+function printName(name) {
+  console.log(`name: ${name}`);
+}
+
+module.exports = { sum, printName, multiply };
